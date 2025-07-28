2f36dd63c5664dc5c4f85366250815076d02af6a
Geovanny Mendoza <me@geovannycode.com>
2025-07-28 12:00:00 -0500

    Commit retroactivo

---
+++ src/test/java/com/geovanycode/CqrsMediatorJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geovanycode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsMediatorJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
